2d1c276c30132a2e9bcdcedf13dad59530fb7554
lucivaldo-brilhante <lucbrilhante@gmail.com>
2020-02-13 17:41:05 +0000

    Generating multiple pdf documents

--- src/Web/Controllers/OrderController.cs
+++ src/Web/Controllers/OrderController.cs
@@ -1,9 +1,12 @@
-﻿using MediatR;
+﻿using System; using System.Net;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.eShopWeb.Web.Features.MyOrders;
 using Microsoft.eShopWeb.Web.Features.OrderDetails;
 using System.Threading.Tasks;
+using IronPdf;
+
 
 namespace Microsoft.eShopWeb.Web.Controllers
 {
@@ -27,6 +30,13 @@ namespace Microsoft.eShopWeb.Web.Controllers
             return View(viewModel);
         }
 
+        [HttpGet("pdf")]
+        public async Task<IActionResult> MyOrdersPdf()
+        {
+            var viewModel = await _mediator.Send(new GetMyOrders(User.Identity.Name));
+
+            return View(viewModel);
+        }
         [HttpGet("{orderId}")]
         public async Task<IActionResult> Detail(int orderId)
         {
@@ -38,6 +48,24 @@ namespace Microsoft.eShopWeb.Web.Controllers
             }
 
             return View(viewModel);
+        }
+
+         [HttpGet("{orderId}/pdf")]
+        public async Task<IActionResult> DetailPdf(int orderId)
+        {
+            var urlBuilder = new System.UriBuilder(new Uri(HttpContext.Request.Scheme + "://" + HttpContext.Request.Host.Value))
+            {
+                Path = Url.Action("Detail", "Pdf", new { orderId = orderId }),
+                Query = null,
+            };
+            var url = urlBuilder.Uri;
+            IronPdf.HtmlToPdf Renderer = new IronPdf.HtmlToPdf();
+            var pdfDoc = await Renderer.RenderUrlAsPdfAsync(url);
+
+            return File(pdfDoc.BinaryData, "application/pdf", $"order{orderId}");
+
+        // var viewResult = await Detail(orderId) as ViewResult;
+            // var renderedView = await viewRenderService.RenderToStringAsync(viewResult.ViewName, viewResult.Model);
         }
     }
 }
